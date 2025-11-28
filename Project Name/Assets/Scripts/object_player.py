e1e9528f18e0ca60963fbaa7f5d7cab58408d7c7
matheus-barbiot <matheusbarbiot01@gmail.com>
2025-11-28 20:33:49 -0300

    object_player.py melhorado

--- Project Name/Assets/Scripts/object_player.py
+++ Project Name/Assets/Scripts/object_player.py
@@ -4,30 +4,54 @@ import InputMapping as Input
 from mathutils import Vector
 from collections import OrderedDict
 
+""" Código inicial de jogador, um template simples para iniciar os projetos no UPBGE. 
+Possui:
+    - movimentos com WASD
+    - Pulo com SPACE
+    - Colisão
+"""
 class Player(bge.types.KX_PythonComponent):
     args = OrderedDict([
-    ('speed', 0.0)
+    ('speed', 0.0) # Parâmetro 'speed' no args que define a velocidade de movimento
     ])
 
+# ================| Init |===============
+
     def start(self, args):
-        self.scene = bge.logic.getCurrentScene()
+        # Componentes necessários para a lógica de jogador
         self.character = bge.constraints.getCharacter(self.object)
         self.object.collisionCallbacks.append(self.collision)
         
+        # Parâmetros do ARGS
         self.speed = args['speed']
-        self.Y = Input.Player_Y
-        self.X = Input.Player_X
-        self.jump = Input.Player_Jump
+        
+        # Entradas de teclado
+        # para movimento:
+        self.Y = Input.Player_Y # Teclas W e S
+        self.X = Input.Player_X # Teclas A e D
+        
+        # para pulo:
+        self.jump = Input.Player_Jump # Tecla SPACE
         pass
     
+# ================| Movimento |===============
+    
+    # Função que aplica o movimento do personagem dependendo de qual tecla está sendo pressionada
     def movement(self):
         vetor = self.object.localOrientation * Vector([self.X.value(), self.Y.value(), 0])
         self.character.walkDirection = vetor.normalized() * self.speed
 
+# ================| Colisão |===============
+
+    # Função do CollisionCallback para verificar colisões
     def collision(self, obj, point, normal):
+        # Colisão com o inimigo
         if 'enemy' in obj:
             self.scene.replace('Death')
             
+# ================| Execução |===============
+            
+    # Atualiza a lógica por todos os quadros por segundo
     def update(self):
         self.movement()
             
@@ -35,3 +59,4 @@ class Player(bge.types.KX_PythonComponent):
             self.character.jump()
         pass
 
+# ================| Final |===================
